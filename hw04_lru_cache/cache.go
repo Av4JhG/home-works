47d8ad376cfb28c674eb2acc436dac186d71fed0
mav_326 <mav@bb.bb>
2025-09-04 16:19:28 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,20 +2,82 @@ package hw04lrucache
 
 type Key string
 
+// Интерфейс для работы с кэшем
 type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
-	Clear()
+	Set(key Key, value interface{}) bool // добавить значение в кэш по ключу.
+	Get(key Key) (interface{}, bool)     // получить значение из кэша по ключу.
+	Clear()                              // очистить кэш.
 }
 
+// Структура элемента кэша.
+type cacheItem struct {
+	key   Key         // ключ элемента кэша.
+	value interface{} // значение элемента кэша.
+}
+
+// Структура LRU кэша.
 type lruCache struct {
-	Cache // Remove me after realization.
-
-	capacity int
-	queue    List
-	items    map[Key]*ListItem
+	capacity int               // емкость кэша
+	queue    List              // очередь
+	items    map[Key]*ListItem // мап элементов кэша.
 }
 
+// Передача значения элемента списка в элемент списка.
+func listItemToCacheItem(item *ListItem) *cacheItem {
+	return item.Value.(*cacheItem)
+}
+
+// Создание элемента кэша.
+func newCacheItem(key Key, value interface{}) *cacheItem {
+	return &cacheItem{key, value}
+}
+
+// Реализация добавления элемента в кэш.
+func (l *lruCache) Set(key Key, value interface{}) bool {
+
+	item, ok := l.items[key]
+
+	if ok {
+		l.queue.MoveToFront(item)
+		listItemToCacheItem(item).value = value
+		return true
+	}
+
+	if l.queue.Len() == l.capacity {
+		removable := l.queue.Back()
+		l.queue.Remove(removable)
+		delete(l.items, listItemToCacheItem(removable).key)
+	}
+
+	item = l.queue.PushFront(newCacheItem(key, value))
+	l.items[key] = item
+
+	return false
+}
+
+// Реализация получения элемента из кэша.
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+
+	item, ok := l.items[key]
+
+	if ok {
+		l.queue.MoveToFront(item)
+		return listItemToCacheItem(item).value, true
+	}
+
+	return nil, false
+}
+
+// Реализация очистки кэша.
+func (l *lruCache) Clear() {
+
+	for l.queue.Len() > 0 {
+		l.queue.Remove(l.queue.Back())
+	}
+	l.items = make(map[Key]*ListItem, l.capacity)
+}
+
+// Функция инициализации кэша (конструктор)
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,26 +1,110 @@
 package hw04lrucache
 
+// Интерфейс работы со списком.
 type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Len() int                          // получение длины списка.
+	Front() *ListItem                  // получение первого элемента списка.
+	Back() *ListItem                   // получение последнего элемента списка.
+	PushFront(v interface{}) *ListItem // добавить значение в начало.
+	PushBack(v interface{}) *ListItem  // добавить значение в конец.
+	Remove(i *ListItem)                // удалить элемент.
+	MoveToFront(i *ListItem)           // переместить элемент в начало.
 }
 
+// Структура элемента списка
 type ListItem struct {
-	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Value interface{} // значение элемента списка.
+	Next  *ListItem   // предыдущий элемент списка.
+	Prev  *ListItem   // следующий элемент списка.
 }
 
+// Структура списка
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int       // длина списка.
+	front *ListItem // первый элемент списка.
+	back  *ListItem // последний элемент списка.
 }
 
+// Реализация получения длины списка.
+func (l *list) Len() int {
+	return l.len
+}
+
+// Реализация получения первого элемента списка.
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+// Реализация получения последнего элемента списка.
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+// Реализация добавления элемента в начало списка.
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{Value: v}
+
+	if l.len == 0 {
+		l.back = item
+	} else {
+		item.Next = l.front
+		l.front.Prev = item
+	}
+	l.front = item
+
+	l.len++
+	return item
+}
+
+// Реализация добавления элемента в конец списка.
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{Value: v}
+
+	if l.len == 0 {
+		l.front = item
+	} else {
+		item.Prev = l.back
+		l.back.Next = item
+	}
+	l.back = item
+
+	l.len++
+	return item
+}
+
+// Реализация удаления элемента списка.
+func (l *list) Remove(i *ListItem) {
+	if i.Prev == nil {
+		l.front = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next == nil {
+		l.back = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	l.len--
+}
+
+// Реализация перемещения элемента в начало списка.
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+
+	l.Remove(i)
+
+	i.Prev = nil
+	i.Next = l.front
+	l.front.Prev = i
+	l.front = i
+	l.len++
+}
+
+// Функция инициализации списка (конструктор)
 func NewList() List {
 	return new(list)
 }
