04e83d3bd2c400fc4aca7153a33c6d5695a3b279
mav_326 <mav@bb.bb>
2025-10-11 14:16:19 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+var ErrCommandInfoNotFound = errors.New("no command provided")
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return int(syscall.EINVAL)
+	}
+
+	setEnvVars(env)
+
+	command := getCommand(cmd)
+	command.Run()
+
+	return command.ProcessState.ExitCode()
+}
+
+func setEnvVars(env Environment) {
+	for name, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(name)
+			continue
+		}
+
+		os.Setenv(name, value.Value)
+	}
+}
+
+func getCommand(cmd []string) *exec.Cmd {
+	command := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	return command
 }
