6a2bc77f6b0604c74f1a5e5596e6335a268b7e46
mav_326 <mav@bb.bb>
2025-11-30 14:33:49 +0300

    HW14 is completed

--- hw12_13_14_15_16_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_16_calendar/cmd/calendar/main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 
 	pb "github.com/fAv4JhG/home-works/hw12_13_14_15_calendar/calendar/pb"
+	config "github.com/fAv4JhG/home-works/hw12_13_14_15_calendar/configs"
 	"github.com/fAv4JhG/home-works/hw12_13_14_15_calendar/internal/app"
 	"github.com/fAv4JhG/home-works/hw12_13_14_15_calendar/internal/logger"
 	internalgrpc "github.com/fAv4JhG/home-works/hw12_13_14_15_calendar/internal/server/grpc"
@@ -33,7 +34,7 @@ func main() {
 		return
 	}
 
-	config, err := NewConfig(configFile)
+	config, err := config.NewConfig(configFile)
 	if err != nil {
 		fmt.Println(configFile)
 		fmt.Println(err)
